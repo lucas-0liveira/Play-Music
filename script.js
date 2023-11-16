ebd3130205afddf187672b7a4f3945dd3fbf5a83
lucas-0liveira <lucas.oliver.70@hotmail.com>
2023-11-16 13:46:32 -0300

    Music

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const play = document.getElementById('play');
 songName.innerText = 'Jet To The West';
 
 function playsong(){
+    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
     song.play();
 }
 
