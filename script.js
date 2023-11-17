470af03a9a8ddc7c50e4f385fb38883e88b65261
lucas-0liveira <lucas.oliver.70@hotmail.com>
2023-11-16 23:01:41 -0300

    Music

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 const songName = document.getElementById('song-name'); 
+const bandName = document.getElementById('band-name');
 const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 
-songName.innerText = 'Jet To The West';
+const JetToTheWest = {
+    songName : 'Jet To The West',
+    artist : 'Lil Mosey',
+    file : 'Jet_To_The_West'
+};
+
+const LuxoNoMorro = {
+    songName : 'LUXO NO MORRO',
+    artist : 'Veigh',
+    file : 'Luxo_No_Morro'
+};
+
+const LouisV = {
+    songName : 'louis v, menina linda ',
+    artist : 'Sidoka',
+    file : 'Louis_V'
+};
+
+const Planos = {
+    songName : 'Planos ft. Nate Shawty! (Prod. Dexhenry & Rxmisz)',
+    artist : 'Pedrwthekid',
+    file : 'planos'
+};
+
+const Sommelier = {
+    songName : 'Sommelier',
+    artist : 'Sidoka',
+    file : 'sommelier'
+};
+
+const Bando = {
+    songName : 'Bando',
+    artist : 'Vinnte$',
+    file : 'bando'
+};
+
 let isPlaying = false;
+
 
 function playsong(){
     play.querySelector('.bi').classList.remove('bi-play-circle-fill');
@@ -13,8 +51,8 @@ function playsong(){
 }
 
 function pausesong(){
-    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
-    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+    play.querySelector('.bi').classList.add('bi-play-circle-fill');
+    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
     song.pause();
     isPlaying = false;
 }
